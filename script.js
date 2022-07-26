04dea1262cc46d46e3cac76cd59581992b27d3ad
pantea <panteaarezaee@gmail.com>
2022-07-26 12:00:37 +0430

    Unit Conversion
    fahranheit to celcius conversion and vice versa available

--- script.js
+++ script.js
@@ -11,78 +11,66 @@ let daylist = [
   "Friday",
   "Saturday",
 ];
-let time = today.getHours() + ":" + today.getMinutes();
-document.getElementById("displayDateTime").innerHTML =
-  daylist[day] + "  " + time;
 
-/* change city name */
+/* search engine */
 
-function showTemperatue(response) {
-  let hightempf = document.querySelector(".hightemp");
-  hightempf.innerHTML = `${Math.round(response.data.main.temp)}°`;
-  let lowtempf = document.querySelector(".lowtemp");
-  lowtempf.innerHTML = `/${Math.round(response.data.main.temp_min)}°`;
+function showTemperature(response) {
+  document.querySelector(".hightemp").innerHTML = `${Math.round(response.data.main.temp)}°`;
+  document.querySelector(".lowtemp").innerHTML = `/${Math.round(response.data.main.temp_min)}°`;
+  document.querySelector("#wind").innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
+  document.querySelector("#humidity").innerHTML = `${Math.round(response.data.main.humidity)} %`;
+  document.querySelector("#status").innerHTML = `${response.data.weather[0].description}`;
+  celsiushightemp = Math.round(response.data.main.temp);
+  celsiuslowtemp = Math.round(response.data.main.temp_min);
 }
-function pressEnter(e) {
-  if (e.key === "Enter") {
-    let cityName = document.querySelector("#city");
-    let city = document.querySelector("#form").value;
-    cityName.innerHTML = city;
 
-    let apiKey = "282e369bc66d56353f526c0bc686be2b";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-    axios.get(apiUrl).then(showTemperatue);
+function search(city){
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+
+}
+
+function changeCitySubmit(event) {
+  event.preventDefault();
+  document.querySelector("#city").innerHTML = document.querySelector("#form").value;
+  search(document.querySelector("#form").value);
+}
+function changeCityEnter(e) {
+  if (e.key === "Enter") {
+  document.querySelector("#city").innerHTML = document.querySelector("#form").value;
+  let city = document.querySelector("#form").value;
+  search(city);
   }
 }
-function changeCity(event) {
-  event.preventDefault();
-  let cityName = document.querySelector("#city");
-  let city = document.querySelector("#form").value;
-  cityName.innerHTML = city;
-  let apiKey = "282e369bc66d56353f526c0bc686be2b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-  axios.get(apiUrl).then(showTemperatue);
-}
-let searchIcon = document.querySelector("#searchicon");
-searchIcon.addEventListener("click", changeCity);
-document.querySelector("#form").addEventListener("keypress", pressEnter);
 
 /* celsius to Fahrenheit and vice versa */
+
 function cToF(event) {
   event.preventDefault();
-  let hightempf = document.querySelector(".hightemp");
-  hightempf.innerHTML = `68°`;
-  let lowtempf = document.querySelector(".lowtemp");
-  lowtempf.innerHTML = `/63°`;
+  document.querySelector("#fahrenheit").classList.add("deactive");
+  document.querySelector("#celsius").classList.remove("deactive");
+  document.querySelector(".hightemp").innerHTML = `${Math.round((celsiushightemp)*9/5 + 32)}°`;
+  document.querySelector(".lowtemp").innerHTML = `/${Math.round((celsiuslowtemp)*9/5 + 32)}°`;
 }
 function fToC(event) {
   event.preventDefault();
-  let hightempc = document.querySelector(".hightemp");
-  hightempc.innerHTML = "20°";
-  let lowtempc = document.querySelector(".lowtemp");
-  lowtempc.innerHTML = "/17°";
-}
-let fahrenheittemperature = document.querySelector(".fahrenheit");
-fahrenheittemperature.addEventListener("click", cToF);
-let celsiustemperature = document.querySelector(".celsius");
-celsiustemperature.addEventListener("click", fToC);
-
-function currentLoc(response) {
-  let hightempf = document.querySelector(".hightemp");
-  hightempf.innerHTML = `${Math.round(response.data.main.temp)}°`;
-  let lowtempf = document.querySelector(".lowtemp");
-  lowtempf.innerHTML = `/${Math.round(response.data.main.temp_min)}°`;
+  document.querySelector("#celsius").classList.add("deactive");
+  document.querySelector("#fahrenheit").classList.remove("deactive");
+  document.querySelector(".hightemp").innerHTML = `${celsiushightemp}°`;;
+  document.querySelector(".lowtemp").innerHTML = `/${celsiuslowtemp}°`;
 }
 
-function handlePosition(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let apiKey = "282e369bc66d56353f526c0bc686be2b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-  axios.get(apiUrl).then(currentLoc);
-}
-function pressButton(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(handlePosition);
-}
-document.querySelector("#currloc").addEventListener("click", pressButton);
+
+document.getElementById("displayDateTime").innerHTML =
+  daylist[day] + "  " + today.getHours() + ":" + today.getMinutes();
+
+document.querySelector("#searchicon").addEventListener("click", changeCitySubmit);
+document.querySelector("#form").addEventListener("keypress", changeCityEnter);
+
+document.querySelector("#fahrenheit").addEventListener("click", cToF);
+document.querySelector("#celsius").addEventListener("click", fToC);
+
+let celsiushightemp = null;
+let celsiuslowtemp = null;
+search("Berlin");
